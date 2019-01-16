2789b8b529db80b645a0d375d6b9084aa8c4f865
Christopher Hoult <choult@datto.com>
2019-01-16 13:47:05 +0000

    Part: Parameterized A

--- test/AdderTest.php
+++ test/AdderTest.php
@@ -1,32 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Choult\UTDR;
 
 class AdderTest extends \PHPUnit\Framework\TestCase
 {
-    public function testAddOneTwoEqualsThree()
+    public function addProvider(): array
     {
-        $adder = new Adder();
-        $this->assertEquals(3, $adder->add(1, 2));
-        $this->assertSame(3, $adder->add(1, 2));
-        $this->assertGreaterThan(2, $adder->add(1, 2));
+        return [
+            [1, 2, 3],
+            [5, 6, 11],
+            [-1, 5, 4],
+            [0, 0, 0]
+        ];
     }
 
-    public function testAddFiveSixEqualsEleven()
+    /**
+     * @dataProvider addProvider
+     */
+    public function testAdd(int $a, int $b, int $expected)
     {
         $adder = new Adder();
-        $this->assertSame(11, $adder->add(5, 6));
-    }
-
-    public function testAddMinusOneFiveEqualsFour()
-    {
-        $adder = new Adder();
-        $this->assertSame(4, $adder->add(-1, 5));
-    }
-
-    public function testAddZeroZeroEqualsZero()
-    {
-        $adder = new Adder();
-        $this->assertSame(0, $adder->add(0, 0));
+        $this->assertEquals($expected, $adder->add($a, $b));
     }
 }
