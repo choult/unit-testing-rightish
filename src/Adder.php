a6913cfa61c5deaf12725aa96a2936425799eddc
Christopher Hoult <choult@datto.com>
2019-01-16 13:53:59 +0000

    Part: Coverage A

--- src/Adder.php
+++ src/Adder.php
@@ -6,8 +6,15 @@ namespace Choult\UTDR;
 
 class Adder
 {
+    private $name;
+
     public function add(int $a, int $b): int
     {
         return $a + $b;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
     }
 }
